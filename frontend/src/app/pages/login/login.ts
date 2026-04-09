304ea0aee02ba93202c3f0ba1e7a6192d922061d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-09 11:07:22 -0300

    feat: adds attributes and methods to frontend/src/app/pages/login/login.ts

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+  private authService = inject(Auth);
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    this.authService.login(this.form.value).subscribe((res: any) => {
+      this.authService.saveToken(res.token);
+    });
+  }
+}
