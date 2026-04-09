eb35da42018df658fd4b848b188c9bfd7e14c979
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-09 11:09:50 -0300

    feat: adds getUsers to frontend/src/app/pages/dashboard/dashboard.ts

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
 })
-export class Dashboard {}
+export class Dashboard {
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    const token = localStorage.getItem('token');
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return this.http.get('http://localhost:5000/api/user', { headers });
+  }
+}
