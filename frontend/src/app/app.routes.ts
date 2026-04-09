5fae69305d76fdf2b1fdadacf8afeb5ba060d311
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-09 11:32:14 -0300

    feat: adds new paths to frontend/src/app/app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    title: 'Dashboard',
+  },
+  {
+    path: 'login',
+    component: Login,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: Register,
+    title: 'Register',
+  },
+];
