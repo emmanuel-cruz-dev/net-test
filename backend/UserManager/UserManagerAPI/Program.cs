95ef2aa95f8a64e1545e9a6641a2ca4ba4f2596a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-10 12:47:51 -0300

    feat: modifies Program.cs in backend/UserManager/UserManagerAPI/

--- backend/UserManager/UserManagerAPI/Program.cs
+++ backend/UserManager/UserManagerAPI/Program.cs
@@ -1,20 +1,14 @@
+using UserManagerAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=users.db"));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
