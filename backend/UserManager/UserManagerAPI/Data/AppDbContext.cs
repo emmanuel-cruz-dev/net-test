72b654894a0eb382a9f828ab4754e7b6c409043c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-10 12:45:14 -0300

    feat: adds AppDbContext.cs to backend/UserManager/UserManagerAPI/Data/

---
+++ backend/UserManager/UserManagerAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagerAPI.Models;
+
+namespace UserManagerAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
