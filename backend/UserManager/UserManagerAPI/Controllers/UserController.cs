d29e8b648b30c67963906353da6c8645d2144cca
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-10 14:06:00 -0300

    feat: adds UserController.cs to backend/UserManager/UserManagerAPI/Controllers/

---
+++ backend/UserManager/UserManagerAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagerAPI.Data;
+using UserManagerAPI.Models;
+
+namespace UserManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+    }
+}
