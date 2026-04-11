f51c5a8e68c64eb99ad809c07be7934939d0ef56
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-11 12:49:33 -0300

    feat: adds JwtService.cs to backend/UserManager/UserManagerAPI/Services/

---
+++ backend/UserManager/UserManagerAPI/Services/JwtService.cs
@@ -0,0 +1,37 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UserManagerAPI.Models;
+
+namespace UserManagerAPI.Services
+{
+    public class JwtService
+    {
+        private readonly string _secretKey;
+
+        public JwtService(IConfiguration configuration)
+        {            
+            _secretKey = configuration["JwtSettings:SecretKey"];
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
