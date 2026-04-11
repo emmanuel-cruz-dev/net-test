f773b107ddb5199444f49537369d1e2b8e8682e9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-11 12:49:00 -0300

    feat: adds authorization to backend/UserManager/UserManagerAPI/Controllers/UserController.cs

--- backend/UserManager/UserManagerAPI/Controllers/UserController.cs
+++ backend/UserManager/UserManagerAPI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using UserManagerAPI.Data;
 using UserManagerAPI.Models;
 
@@ -6,6 +7,7 @@ namespace UserManagerAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly AppDbContext _context;
